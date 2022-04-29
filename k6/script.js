fe4d98c43268cea833f2728957685bf301e44609
Adarshraj Ravindran <adarshrajrav@gmail.com>
2022-04-28 23:56:18 -0700

    ready to deploy

--- k6/script.js
+++ k6/script.js
@@ -4,11 +4,11 @@ import { check, sleep } from 'k6';
 export const options = {
   stages: [
     { duration: '30s', target: 20 },
-    { duration: '1m30s', target: 100 },
-    { duration: '20s', target: 0 },
+    { duration: '1m30s', target: 1000 },
+    { duration: '20s', target: 200 },
   ],
 };
 
 export default () => {
-  http.get(`http://localhost:3000/meta?product_id=${Math.floor(Math.random() * 1000000)}`)
+  http.get(`http://localhost:3000/?product_id=${Math.floor(Math.random() * 1000000)}`)
 }
